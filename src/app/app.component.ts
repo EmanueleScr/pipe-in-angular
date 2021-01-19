75e0ed02db123fa69b61975a5bd36dc4ad5d4956
EmanueleScr <emanuele.scrocca@gmail.com>
2021-01-19 12:36:53 +0100

    base project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  
+  uppercase_value:string = '';
+  datepipe_value:string = '';
+  currency_value:number;
+
+  onUppercaseInput(e:string){
+    this.uppercase_value = e;
+  }
+
+  onDateInput(e:string){
+    this.datepipe_value = e;
+  }
+
+  onCurrencyInput(e:string){
+    this.currency_value = parseInt(e);
+  }
+
+  car:object = {
+    marca: 'Tesla',
+    modello: 'Model 3',
+    colore: 'Rosso',
+    accelerazione(){
+      console.log('velocità massima')
+    }
+  }
+
 }
